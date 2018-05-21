ea13c243ce73f36ac0f909c3805271539fcb24ce
PcCodeWarrior <PC_Code_Warrior@yahoo.com>
2018-05-21 11:48:51 -0600

    add pictures

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -5,6 +5,7 @@ const Panel = (props) => {
     return (
         <div className={props.location}>
             <h1>{props.name}</h1>
+            <img class="center" alt='robots' src={`https://robohash.org/${props.name}?size=200x200`}/>
             <button  className="buttonFetch" onClick={()=> props.cv(props.index)}>Change State</button>
         </div>
     );
