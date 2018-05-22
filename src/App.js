62336fd873b48cbae6e6737d12b256426a73578e
pccodewarrior <pc_code_warrior@yahoo.com>
2018-05-21 18:08:31 -0600

    make the code pretty

--- src/App.js
+++ src/App.js
@@ -6,33 +6,35 @@ import axios from "axios/index";
 
 class App extends Component {
 
-    state = {panels: [
-            {id: 'Left', name:'Left'},
-            {id: 'Middle', name:'Middle'},
-            {id: 'Right',name:'Right'},
-            {id: 'Bottom',name:'Bottom'}
-    ]};
+    state = {
+        panels: [
+            {id: 'Left', name: 'Left'},
+            {id: 'Middle', name: 'Middle'},
+            {id: 'Right', name: 'Right'},
+            {id: 'Bottom', name: 'Bottom'}
+        ]
+    };
 
 
     changeVal = (index) => {
-        const current =this.state.panels[index];
+        const current = this.state.panels[index];
         axios.get("https://talaikis.com/api/quotes/random/ ")
             .then(response => {
-                current.name=response.data.author;
+                current.name = response.data.author;
                 this.setState(this.state.panels);
             })
-            .catch(error =>{console.log(error);
+            .catch(error => {
+                console.log(error);
             });
     };
 
-render()
-{
-    return (
-        <div className="App">
-            <Panels p={this.state.panels} cv={this.changeVal}/>
-        </div>
-    );
-}
+    render() {
+        return (
+            <div className="App">
+                <Panels p={this.state.panels} cv={this.changeVal}/>
+            </div>
+        );
+    }
 }
 
 export default App;
